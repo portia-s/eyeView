b0e76b4e752ae44399397de87405ca27ae2e6796
Portia <portiasharma@portia-mbpro.local>
2017-09-01 14:13:10 -0700

    pod bridgingHeader & path@buildSetings LocationAlwaysUsage@infoPlist backgroundBLE@projectTargetCapabilities

---
+++ eyeView/Model.swift
@@ -0,0 +1,9 @@
+//
+//  Model.swift
+//  eyeView
+//
+//  Created by Portia Sharma on 9/1/17.
+//  Copyright © 2017 Portia Sharma. All rights reserved.
+//
+
+import Foundation
---
+++ eyeView/ObjCBridge.h
@@ -0,0 +1,15 @@
+//
+//  ObjCBridge.h
+//  eyeView
+//
+//  Created by Portia Sharma on 9/1/17.
+//  Copyright © 2017 Portia Sharma. All rights reserved.
+//
+
+#import <EstimoteSDK/EstimoteSDK.h>
+
+#ifndef ObjCBridge_h
+#define ObjCBridge_h
+
+
+#endif /* ObjCBridge_h */
